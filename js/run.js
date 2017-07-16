c0d101dbdc73dacb077d0f89e065e1a9dca21e34
Asiazdx <2081048066@qq.com>
2017-07-17 01:10:45 +0800

    粒子动画流血部分处理

--- js/run.js
+++ js/run.js
@@ -1,4 +1,13 @@
-window.onload=function(){
+$(function(){
+    //  $("img").load(function(){
+    //     console.log('yijiazai');
+    // })
+    
+    setTimeout(function(){
+        $("#box").css({
+            left:-2000
+        });
+    },3000);
     var clientw=document.documentElement.clientWidth;
     var clienth=document.documentElement.clientHeight;
     var canvas=document.querySelector("canvas");
@@ -28,4 +37,36 @@ window.onload=function(){
 
     })
 
-};
+})
+
+// window.onload=function(){
+//     var clientw=document.documentElement.clientWidth;
+//     var clienth=document.documentElement.clientHeight;
+//     var canvas=document.querySelector("canvas");
+//     canvas.width=clientw;
+//     canvas.height=clienth;
+//     var cobj=canvas.getContext("2d");
+//     var runs=document.querySelectorAll(".run");
+//     var jumps=document.querySelectorAll(".jump");
+//     var hinderImg=document.querySelectorAll(".hinder");
+
+//     var runA=document.querySelector(".runA");
+//     var zidanImg=document.querySelectorAll(".zidan");
+//     var zidanA=document.querySelector(".zidanA");
+//     var jumpA=document.querySelector(".jumpA");
+
+//     var dieA=document.querySelector(".dieA");
+//     var progress=document.querySelector(".progress1");
+//     var jifen=document.querySelector(".jifen");
+//     var gameObj=new game(canvas,cobj,runs,jumps,hinderImg,runA,zidanA,jumpA,dieA,progress,jifen,zidanImg);
+//     // 选项卡
+//     var start=$(".start");
+//     // 遮罩
+//     var mask=$(".mask");
+//     //开始按钮
+//     $(".ksyx").one("click",function(){
+//         gameObj.play(start,mask);
+
+//     })
+
+// };
